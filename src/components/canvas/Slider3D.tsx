392f5a5edbaf77f83a3851e5ac4893aad4153072
Rik <rikrooij.de@gmail.com>
2022-06-13 23:32:43 +0200

    ts ignore

--- src/components/canvas/Slider3D.tsx
+++ src/components/canvas/Slider3D.tsx
@@ -1,12 +1,12 @@
-import useStore from "@/helpers/store"
-import { useSpring, animated } from "@react-spring/three"
-import { act, useThree } from "@react-three/fiber"
-import { useDrag } from "@use-gesture/react"
-import { useEffect, useRef, useState } from "react"
-import { Euler, Plane, Vector3 } from "three"
-import { clamp } from "three/src/math/MathUtils"
+import useStore from '@/helpers/store'
+import { useSpring, animated } from '@react-spring/three'
+import { act, useThree } from '@react-three/fiber'
+import { useDrag } from '@use-gesture/react'
+import { useEffect, useRef, useState } from 'react'
+import { Euler, Plane, Vector3 } from 'three'
+import { clamp } from 'three/src/math/MathUtils'
 
-export interface Slider3DProps{
+export interface Slider3DProps {
   origin: Vector3
   orientation: Euler
   direction: string
@@ -14,7 +14,7 @@ export interface Slider3DProps{
   min: number
   max: number
   floorPlane: Plane
-  setIsSliding: (value: boolean) => void 
+  setIsSliding: (value: boolean) => void
 }
 
 const Slider3D = ({
@@ -25,7 +25,7 @@ const Slider3D = ({
   min,
   max,
   floorPlane,
-  setIsSliding
+  setIsSliding,
 }: Slider3DProps) => {
   const thumbPos = -((useStore((state) => state.width) / max) * length)
   const xPos = -origin.x
@@ -37,31 +37,32 @@ const Slider3D = ({
   const [spring, api] = useSpring(() => ({
     position: pos,
     scale: 1,
-    config: { friction: 12 }
-  }));
+    config: { friction: 12 },
+  }))
 
   api.start({
-     position: pos,
-    scale: dragging ? 0.9 : hovered ? 1.2 : 1
+    position: pos,
+    scale: dragging ? 0.9 : hovered ? 1.2 : 1,
   })
 
-   let planeIntersectPoint = new Vector3();
-   const bind = useDrag(
+  let planeIntersectPoint = new Vector3()
+  const bind = useDrag(
     ({ active, timeStamp, event }) => {
       if (active) {
-        event.ray.intersectPlane(floorPlane, planeIntersectPoint);
+        // @ts-ignore
+        event.ray.intersectPlane(floorPlane, planeIntersectPoint)
 
         const xPosition = clamp(planeIntersectPoint.x - origin.x, -length, 0)
         const localPosition = [xPosition, 0, 0]
-        setPos(localPosition);
+        setPos(localPosition)
 
         const sliderValue = clamp((Math.abs(pos[0]) / 3) * 10, 0.1, 10)
-        useStore.setState({width: sliderValue})
+        useStore.setState({ width: sliderValue })
       }
 
       setIsSliding(active)
       setDragging(active)
-      return timeStamp;
+      return timeStamp
     },
     { delay: false }
   )
@@ -70,24 +71,39 @@ const Slider3D = ({
     <>
       <group position={origin}>
         {/* Track: */}
-        <mesh castShadow position={[xPos, 0, 0]} rotation={[0, 0, Math.PI * 0.5]}>
-          <cylinderBufferGeometry args={[0.025, 0.025, length + 0.2, 16, 5]}  />
-          <meshBasicMaterial color="black" />
+        <mesh
+          castShadow
+          position={[xPos, 0, 0]}
+          rotation={[0, 0, Math.PI * 0.5]}
+        >
+          <cylinderBufferGeometry args={[0.025, 0.025, length + 0.2, 16, 5]} />
+          <meshBasicMaterial color='black' />
         </mesh>
         {/* Thumb */}
-        <animated.mesh castShadow {...spring} {...bind()} onPointerOver={() => {setHovered(true)}} onPointerLeave={() => {setHovered(false)}} >
+        {/* @ts-ignore */}
+        <animated.mesh
+          castShadow
+          {...spring}
+          {...bind()}
+          onPointerOver={() => {
+            setHovered(true)
+          }}
+          onPointerLeave={() => {
+            setHovered(false)
+          }}
+        >
           <boxBufferGeometry args={[0.25, 0.25, 0.25]} />
-          <meshBasicMaterial color="red"/>
+          <meshBasicMaterial color='red' />
         </animated.mesh>
         {/* Left border */}
         <mesh castShadow position={[0.15, 0, 0]}>
           <boxBufferGeometry args={[0.1, 0.2, 0.2]} />
-          <meshBasicMaterial color="black" />
+          <meshBasicMaterial color='black' />
         </mesh>
         {/* Right border */}
         <mesh castShadow position={[-length - 0.15, 0, 0]}>
           <boxBufferGeometry args={[0.1, 0.2, 0.2]} />
-          <meshBasicMaterial color="black" />
+          <meshBasicMaterial color='black' />
         </mesh>
       </group>
     </>
